be5f12cbe4b7e145f4f1d57d3e179c17f418905d
Saumi-Pandya <saumipandya@gmail.com>
2021-06-02 17:56:48 +0530

    setting up flatlist

--- App.js
+++ App.js
@@ -1,15 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{Component} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export default class App extends Component {
+
+  state = {data:[]};
+
+  componentDidMount(){
+    this.fetchData();
+  }
+
+  fetchData = async()=>{
+      const response = await fetch(' https://randomuser.me/api/?results=100&inc=name')
+      const jsonObj = await response.json();
+      this.setState({data:jsonObj.results});
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.data}
+          keyExtractor={(x, i) => i}
+          renderItem={({ item }) =>
+            <Text>
+              {`${item.name.title} ${item.name.first} ${item.name.last}`}
+            </Text>}
+        />
+      </View>
+    );
+  }
+  
+};
 
 const styles = StyleSheet.create({
   container: {
